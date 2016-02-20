537ca5b8cd51c81a5a7654a6e219622a10ed3d16
Rodrick <rodkd@live.com>
2016-02-19 19:39:49 -0500

    most recent timeseries implementation and start of most recent lab

--- TimeSeries_RKD.py
+++ TimeSeries_RKD.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 class TimeSeries():
     """ 
@@ -14,9 +15,16 @@ class TimeSeries():
     __str___
     --------
     Returns a string for the output of the print statement acting on an instance of the 
-    TimeSeries class that abbreviates the output by only printing the first 20 elements 
-    followed by an ellipsis if the list used as input for the class has more than 20 elements.
-    Otherwise, the output of the print statement uses the whole list.
+    TimeSeries class that abbreviates the output by only printing the first 10 elements 
+    followed by an ellipsis and the length of the list if the list used as input for the 
+    class has more than 10 elements. Otherwise, the output of the print statement uses 
+    the whole list.
+    
+    Examples
+    --------
+    >>> threes = TimeSeries(range(0,10000,3))
+    >>> print(threes)
+    TimeSeries: [0, 3, 6, 9, 12, 15, 18, 21, 24, 27, ...], length=3334
     """
     def __init__(self, data):
         self.data = list(data)
@@ -25,11 +33,8 @@ class TimeSeries():
         return len(self.data)
     
     def __getitem__(self, position):
-        if position < len(self) and position >= 0:
-            return (self.data[position])
-        else:
-            return -1
-    
+        return (self.data[position])
+        
     def __setitem__(self, position, value):
         if position < len(self) and position >= 0:
             self.data[position] = value
@@ -38,12 +43,11 @@ class TimeSeries():
     
     def __str__(self):
         class_name = type(self).__name__
-        if len(self) > 20:
-            components = self.data[:20]
+        if len(self) > 10:
+            components = self.data[:10]
             string = '{}: ['.format(class_name)
-            for j in range(20):
-                string += '{}, '.format(components[j])
-            string += '...]'
+            string += ('{}, '*10).format(*components)
+            string += '...], length = {}'.format(len(self))
             return string
         else:
             components = self.data
@@ -52,14 +56,17 @@ class TimeSeries():
     
     def __repr__(self):
         class_name = type(self).__name__
-        if len(self) > 20:
-            components = self.data[:20]
+        if len(self) > 10:
+            components = self.data[:10]
             string = '{}(['.format(class_name)
-            for j in range(20):
-                string += '{}, '.format(components[j])
-            string += '...])'
+            string += ('{}, '*10).format(*components)
+            string += '...], length = {})'.format(len(self))
             return string
         else:
             components = self.data
             return '{}({})'.format(class_name, components)
+
+class ArrayTimeSeries(TimeSeries):
+    def __init__(self,data):
+        self.data=np.array(data)
     
