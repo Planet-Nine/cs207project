fdae684689bd9d957bc92507998c6d8064b2f405
Sarah Wellons <sarah@Sarahs-MacBook-Air-4.local>
2016-03-17 02:22:28 -0400

    filled out parser methods, tests successfully on samples

---
+++ pype/parser.py
@@ -0,0 +1,126 @@
+import ply.yacc
+
+from .lexer import tokens,reserved
+from .ast import *
+
+# Here's an example production rule which constructs an AST node
+def p_program(p):
+  r'program : statement_list'
+  p[0] = ASTProgram(p[1])
+
+# Here's an example production rule which simply aggregates lists of AST nodes.
+def p_statement_list(p):
+  r'''statement_list : statement_list component
+                     | statement_list import_statement
+                     | import_statement
+                     | component'''
+  if len(p)>2:
+    p[1].append(p[2])
+    p[0] = p[1]
+  else:
+    p[0] = [p[1]]
+
+def p_import_statement(p):
+  r'import_statement : LPAREN IMPORT ID RPAREN'
+  p[0] = ASTImport(p[3])
+
+def p_component(p):
+  r'''component : LBRACE ID expression_list RBRACE'''
+  p[0] = ASTComponent(p[2], p[3])
+
+def p_expression_list(p):
+  r'''expression_list : expression_list expression
+                      | expression'''
+  if len(p)>2:
+    p[1].append(p[2])
+    p[0] = p[1]
+  else:
+    p[0] = [p[1]]
+
+def p_input(p):
+  r'''expression : LPAREN INPUT declaration_list RPAREN
+                 | LPAREN INPUT RPAREN'''
+  if len(p) > 4:
+      p[0] = ASTInputExpr(p[3])
+  else:
+      p[0] = ASTInputExpr()
+
+def p_output(p):
+  r'''expression : LPAREN OUTPUT declaration_list RPAREN
+                 | LPAREN OUTPUT RPAREN'''
+  if len(p) > 4:
+      p[0] = ASTOutputExpr(p[3])
+  else:
+      p[0] = ASTOutputExpr()
+
+def p_declaration_list(p):
+  r'''declaration_list : declaration_list declaration
+                       | declaration'''
+  if len(p)>2:
+    p[1].append(p[2])
+    p[0] = p[1]
+  else:
+    p[0] = [p[1]]
+
+def p_declaration(p):
+  r'''declaration : LPAREN type ID RPAREN
+                  | ID'''
+  if len(p) > 2:
+      p[0] = ASTID(p[3], typedecl=p[2])
+  else:
+      p[0] = ASTID(p[1])
+
+# How does it know the difference between this and lines 67 and 95?
+def p_type(p):
+  r'''type : ID'''
+  p[0] = p[1]
+
+def p_assign(p):
+  r'''expression : LPAREN ASSIGN ID expression RPAREN'''
+  p[0] = ASTAssignmentExpr(p[3], p[4])
+
+def p_funcexpr(p):
+  r'''expression : LPAREN ID parameter_list RPAREN'''
+  p[0] = ASTEvalExpr(p[2],p[3])
+
+def p_dontunderstand(p):
+  r'''expression : LPAREN ID RPAREN'''
+  p[0] = p[2]
+
+def p_op(p):
+  r'''expression : LPAREN OP_ADD parameter_list RPAREN
+                 | LPAREN OP_SUB parameter_list RPAREN
+                 | LPAREN OP_MUL parameter_list RPAREN
+                 | LPAREN OP_DIV parameter_list RPAREN'''
+  p[0] = ASTEvalExpr(p[2], p[3])
+
+def p_exprid(p):
+  r'''expression : ID'''
+  p[0] = ASTID(p[1])
+
+def p_literal(p):
+  r'''expression : NUMBER
+                 | STRING'''
+  p[0] = ASTLiteral(p[1])
+
+def p_parameter_list(p):
+  r'''parameter_list : parameter_list expression
+                     | expression'''
+  if len(p)>2:
+    p[1].append(p[2])
+    p[0] = p[1]
+  else:
+    p[0] = [p[1]]
+
+# TODO: Write an error handling function. You should attempt to make the error
+#       message sensible. For instance, print out the line and column numbers to
+#       help find your error.
+# NOTE: You do NOT need to write production rules with error tokens in them.
+#       If you're interested, read section 6.8, but it requires a fairly deep
+#       understanding of LR parsers and the language specification.
+def p_error(p):
+    pass
+
+start = 'program'
+parser = ply.yacc.yacc() # To get more information, add debug=True
+
