4e905b4e813271f52091d138394417a03eae721d
Rodrick <rodkd@live.com>
2016-03-22 13:09:47 -0400

    added checking for input expressions and for different components

--- pype/semantic_analysis.py
+++ pype/semantic_analysis.py
@@ -16,15 +16,27 @@ class CheckSingleAssignment(ASTVisitor):
   def __init__(self):
     self.pairs = []
     self.component = None
+    self.components = []
     pass # TODO
   def visit(self, node):
     if isinstance(node,ASTComponent):
         self.component = node.name.name
-        self.pairs = []
+        if self.component in self.components:
+            raise SyntaxError("Multiple assignment of component '%s' is not supported"%self.component)
+        else:
+            self.components += [self.component] 
+        self.pairs = [(self.component,self.component)]
     if isinstance(node,ASTAssignmentExpr):
         pair = (node.binding.name,self.component)
         if pair in self.pairs:
-            raise SyntaxError("Multiple assignment of '%s' in component '%s' is not supported"%(pair[0],pair[1]))
+            raise SyntaxError("Multiple assignment of '%s' in component '%s' is not supported"%pair)
         else:
-            self.pairs += [pair]            
+            self.pairs += [pair]     
+    if isinstance(node,ASTInputExpr):
+        for child in node.children:
+            pair = (child.name,self.component)
+            if pair in self.pairs:
+                raise SyntaxError("Multiple assignment of '%s' in component '%s' is not supported"%pair)
+            else:
+                self.pairs += [pair] 
     # TODO
