6cc8c022f81b4a1af50433d67a29488786966229
Rodrick <rodkd@live.com>
2016-03-31 22:23:06 -0400

    test for dead code elimination

--- timeseries/tests/test_pype.py
+++ timeseries/tests/test_pype.py
@@ -118,8 +118,8 @@ class MyTest(unittest.TestCase):
           (output c)
         }
         """
-        graph1 = 'digraph six {\n  "@N10" -> "@N11"\n  "@N11" -> "@N19"\n  "@N11" [ label = "c" ]\n  "@N1" [ label = "y" ]\n  "@N0" [ label = "x" ]\n  "@N0" [ color = "green" ]\n  "@N1" [ color = "green" ]\n  "@N19" [ color = "red" ]\n}\n'
-        graph2 = 'digraph component2 {\n  "@N2" -> "@N4"\n  "@N3" -> "@N4"\n  "@N5" -> "@N6"\n  "@N1" -> "@N3"\n  "@N1" -> "@N3"\n  "@N0" -> "@N2"\n  "@N0" -> "@N2"\n  "@N4" -> "@N5"\n  "@N1" [ label = "y" ]\n  "@N5" [ label = "z" ]\n  "@N0" [ label = "x" ]\n  "@N0" [ color = "green" ]\n  "@N1" [ color = "green" ]\n  "@N6" [ color = "red" ]\n}\n'
+        graph1 = 'digraph six {\n  "@N10" -> "@N11"\n  "@N11" -> "@N19"\n  "@N1" [ label = "y" ]\n  "@N0" [ label = "x" ]\n  "@N11" [ label = "c" ]\n  "@N0" [ color = "green" ]\n  "@N1" [ color = "green" ]\n  "@N19" [ color = "red" ]\n}\n'
+        graph2 = 'digraph component2 {\n  "@N2" -> "@N4"\n  "@N3" -> "@N4"\n  "@N1" -> "@N3"\n  "@N1" -> "@N3"\n  "@N0" -> "@N2"\n  "@N0" -> "@N2"\n  "@N4" -> "@N5"\n  "@N5" -> "@N6"\n  "@N5" [ label = "z" ]\n  "@N0" [ label = "x" ]\n  "@N1" [ label = "y" ]\n  "@N0" [ color = "green" ]\n  "@N1" [ color = "green" ]\n  "@N6" [ color = "red" ]\n}\n'
         ast = pype.parser.parser.parse(data,pype.lexer.lexer)
         q = pype.translate.SymbolTableVisitor()
         ast.walk(q)
